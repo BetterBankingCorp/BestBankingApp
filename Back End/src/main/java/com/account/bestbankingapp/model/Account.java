204d28486cab3efd158f37162b7daf80eec4b128
harmerr <101577717+harmerr@users.noreply.github.com>
2022-05-31 12:39:54 -0400

    Added inheritance to Account model classes for database

--- Back End/src/main/java/com/account/bestbankingapp/model/Account.java
+++ Back End/src/main/java/com/account/bestbankingapp/model/Account.java
@@ -8,80 +8,101 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "account")
-public abstract class Account implements IBaseRate {
-	
+@Table(name = "accounts")
+public abstract class Account {
+
 	@Id
-	@GeneratedValue(strategy =GenerationType.AUTO)
-	@Column(name="id")
-	private String name;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	// Attributes
 	@Column
 	private String ssn;
 	@Column
-	private  double balance;
+	private long accountNum;
 	@Column
-	protected double rate;
+	private double balance;
 	@Column
-	protected String accountNum;
+	protected String name;
 	@Column
 	private static int index = 10000;
-	
-	
+
+	// Getters & Setters
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getSsn() {
 		return ssn;
 	}
+
 	public void setSsn(String ssn) {
 		this.ssn = ssn;
 	}
+
 	public double getBalance() {
 		return balance;
 	}
+
 	public void setBalance(double balance) {
 		this.balance = balance;
 	}
+
 	public double getRate() {
 		return rate;
 	}
+
 	public void setRate(double rate) {
 		this.rate = rate;
 	}
+
 	public String getAccountNum() {
 		return accountNum;
 	}
+
 	public void setAccountNum(String accountNum) {
 		this.accountNum = accountNum;
 	}
+
 	public static int getIndex() {
 		return index;
 	}
+
 	public static void setIndex(int index) {
 		Account.index = index;
 	}
-	public Account(String name, String ssn, double balance, double rate, String accountNum) {
-		super();
+
+	// Constructors
+	public Account(String name, String SSN, double initDeposit) {
 		this.name = name;
-		this.ssn = ssn;
-		this.balance = balance;
-		this.rate = rate;
-		this.accountNum = accountNum;
+		this.SSN = SSN;
+		balance = initDeposit;
+
+		index++;
+		this.accountNumber = setAccountNumber();
+
+		setRate();
 	}
 
 	public Account() {
-		
+
 	}
+
+	// Determines each person's account number
+	private long setAccountNumber() {
+		String lastTwoOfSSN = SSN.substring(SSN.length() - 2, SSN.length());
+		int uniqueID = index;
+		int randomNumber = (int) (Math.random() * Math.pow(10, 3));
+		return lastTwoOfSSN + uniqueID + randomNumber;
+	}
+
 	@Override
 	public String toString() {
 		return "Account [name=" + name + ", ssn=" + ssn + ", balance=" + balance + ", rate=" + rate + ", accountNum="
 				+ accountNum + "]";
 	}
-	
-	
-}
-	
 
+}
