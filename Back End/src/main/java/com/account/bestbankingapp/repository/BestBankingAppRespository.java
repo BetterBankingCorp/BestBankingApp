c8b8695adfdaa4bd48dd525e59be156f44c125b7
RehnaAnn <rehna.anthru@gmail.com>
2022-05-31 13:40:35 -0400

    created new repo

--- Back End/src/main/java/com/account/bestbankingapp/repository/BestBankingAppRespository.java
+++ Back End/src/main/java/com/account/bestbankingapp/repository/BestBankingAppRespository.java
@@ -1,5 +1,17 @@
 package com.account.bestbankingapp.repository;
+import java.util.List;
 
-public class BestBankingAppRespository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.account.bestbankingapp.model.Account;
+
+
+@Repository
+public interface BestBankingAppRespository extends JpaRepository <Account, Long>{
+@Query("select per.id,per.name from Account per")
+	
+	public List<Account> getList();
 
 }
