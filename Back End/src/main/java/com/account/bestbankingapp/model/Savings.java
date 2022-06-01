21ba265b87d960c7460f4860fc533f1f5a03436e
Shane C <99273878+ShaneCupid@users.noreply.github.com>
2022-06-01 06:42:06 -0700

    Revert "some updates to make CRUD operations"

--- Back End/src/main/java/com/account/bestbankingapp/model/Savings.java
+++ Back End/src/main/java/com/account/bestbankingapp/model/Savings.java
@@ -4,50 +4,25 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-import com.account.bestbankingapp.model.Account;
-
 @Entity
-@Table(name = "account")
-public class Savings extends Account {
-	@Column(name = "safetyDepositBoxID") 
-	private int safetyDepositBoxID;
-	@Column(name = "safetyDepositBoxKey")
-	private int safetyDepositBoxKey;
-	
+public class Savings extends Account implements IBaseRate {
+
+	@Column
+	protected double rate;
+
 	public Savings() {
+	}
+
+	public Savings(String name, String ssn, double balance) {
+		super(name, ssn, balance);
+		accountNum = "1" + accountNum;
 		
-	}
-	
-	public Savings(String name, String ssn, double balance, double rate, String accountNum, int safetyDepositBoxID,
-			int safetyDepositBoxKey) {
-		super(name, ssn, balance, rate, accountNum);
-		this.safetyDepositBoxID = safetyDepositBoxID;
-		this.safetyDepositBoxKey = safetyDepositBoxKey;
-	}
-
-	public int getSafetyDepositBoxID() {
-		return safetyDepositBoxID;
-	}
-
-	public void setSafetyDepositBoxID(int safetyDepositBoxID) {
-		this.safetyDepositBoxID = safetyDepositBoxID;
-	}
-
-	public int getSafetyDepositBoxKey() {
-		return safetyDepositBoxKey;
-	}
-
-	public void setSafetyDepositBoxKey(int safetyDepositBoxKey) {
-		this.safetyDepositBoxKey = safetyDepositBoxKey;
+		this.rate = rate;
 	}
 
 	@Override
 	public String toString() {
-		return "Savings [safetyDepositBoxID=" + safetyDepositBoxID + ", safetyDepositBoxKey=" + safetyDepositBoxKey
-				+ ", rate=" + rate + ", accountNum=" + accountNum + "]";
+		return "Savings [rate = " + rate + " , accountNum = " + "]";
 	}
-	
-	
 
 }
-
