a901a98cdcb4ff5ea3a9a62b5f034ad09f3b7d64
Raymond Harmer <101577717+harmerr@users.noreply.github.com>
2022-06-03 11:07:37 -0400

    Added cross origin Spring annotation

--- Back End/src/main/java/com/account/bestbankingapp/controller/BestBankingAppController.java
+++ Back End/src/main/java/com/account/bestbankingapp/controller/BestBankingAppController.java
@@ -26,39 +26,49 @@ public class BestBankingAppController {
 	@Autowired
     BestBankingAppService accountService;
 	
+	@CrossOrigin(origins = "*")
 	@RequestMapping(value="/accounts/savings", method=RequestMethod.POST)
 	public Savings createSavings(@RequestBody Savings savingsAccount) {
 	    return accountService.createSavingsAccount(savingsAccount);
 	}
 	
+	@CrossOrigin(origins = "*")
 	@RequestMapping(value="/accounts/checking", method=RequestMethod.POST)
 	public Checking createChecking(@RequestBody Checking checkingAccount) {
 	    return accountService.createCheckingAccount(checkingAccount);
 	}
 	
+	@CrossOrigin(origins = "*")
 	@RequestMapping(value="/accounts", method=RequestMethod.GET)
 	public List<Account> readAccounts() {
 	    return accountService.getAccounts();
 	}
+	
+	@CrossOrigin(origins = "*")
 	@PatchMapping("/accounts/{accountNum}")
 	public Account patchAccount(@PathVariable(value = "accountNum") String accountNum, @PathVariable(value = "newname") String newName) {
 		return accountService.patchAccount(accountNum, newName);
 	}
 	
+	@CrossOrigin(origins = "*")
 	@RequestMapping(value="/accounts/{accountNum}", method=RequestMethod.DELETE)
 	public void deleteAccount(@PathVariable(value = "accountNum") String accountNum) {
 		accountService.deleteAccount(accountNum);
 	}
+	
+	@CrossOrigin(origins = "*")
 	@RequestMapping(value="/accounts/savings/{accountNum}", method=RequestMethod.PUT)
 	public Account readAccounts(@PathVariable(value = "accountNum") String AccountNum, @RequestBody Savings accountDetails) {
 	    return accountService.updateSavingsAccount(AccountNum, accountDetails);
 	}
 	
+	@CrossOrigin(origins = "*")
 	@PatchMapping("/accounts/deposit/{accountNum}/{depositAmount}")
 	public Account depositAccount(@PathVariable(value = "accountNum") String accountNum,  @PathVariable(value = "depositAmount") double depositAmount) {
 		return accountService.depositAccount(accountNum, depositAmount);
 	}
 	
+	@CrossOrigin(origins = "*")
 	@PatchMapping("/accounts/withdraw/{accountNum}/{withdrawAmount}")
 	public Account withdrawAccount(@PathVariable(value = "accountNum") String accountNum,  @PathVariable(value = "withdrawAmount") double withdrawAmount) {
 		return accountService.withdrawAccount(accountNum, withdrawAmount);
